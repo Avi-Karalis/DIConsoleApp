138f02c5a7f1c24cc710b90d3a2bc26f57b4dad3
Avi-Karalis <averkkaralis@gmail.com>
2023-07-19 14:56:28 +0300

    .

--- DIConsoleApp/Program.cs
+++ DIConsoleApp/Program.cs
@@ -13,7 +13,7 @@ namespace DiConsoleApp;
 internal partial class Program {
     private static void Main(string[] args) {
         var builder = new ConfigurationBuilder();
-        BuildConfig(builder);
+        ProgramHelpers.BuildConfig(builder);
 
         var configuration = builder.Build();
 
@@ -26,8 +26,6 @@ internal partial class Program {
         Log.Logger.Information("Application Starting");
 
         try {
-
-
             var serviceProvider = new ServiceCollection()
                 .AddLogging()
                 .AddSingleton<IConfiguration>(configuration)
@@ -44,20 +42,13 @@ internal partial class Program {
             bar.DoSomeWork();
 
             logger.LogDebug("Job's Done");
+
         } catch (Exception ex) {
             Log.Fatal(ex, "Oops something crashed");
+
         } finally {
             Log.CloseAndFlush();
-        }
-    }
 
-    // configure the logger. and pass the refernece of the configbuilder
-    private static void BuildConfig(IConfigurationBuilder configBuilder) {
-        //search your current directory for JSON. It's mandatory, and you must reload it anytime it is changed
-        configBuilder.SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", false, true)
-            //if there is a developmentjson use is otherwise use the one you have.
-            .AddJsonFile($"appsetings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true)
-            .AddEnvironmentVariables();
+        }
     }
 }
---
+++ DIConsoleApp/ProgramHelpers.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+internal static class ProgramHelpers {
+
+    // configure the logger. and pass the refernece of the configbuilder
+    internal static void BuildConfig(IConfigurationBuilder configBuilder) {
+        //search your current directory for JSON. It's mandatory, and you must reload it anytime it is changed
+        configBuilder.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", false, true)
+            //if there is a developmentjson use is otherwise use the one you have.
+            .AddJsonFile($"appsetings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true)
+            .AddEnvironmentVariables();
+    }
+}
